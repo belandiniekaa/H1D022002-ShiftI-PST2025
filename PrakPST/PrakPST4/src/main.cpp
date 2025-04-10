ba02f606a42f68e3a969a6672ca9cc5ce15f3c77
Eka Belandini <eka.belandini@mhs.unsoed.ac.id>
2025-04-10 11:29:22 +0700

    Pertemuan 4 - Servo

---
+++ PrakPST/PrakPST4/src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo myservo;
+
+void setup() {
+  myservo.attach(D1);
+}
+
+// void loop() {
+//   for (int pos = 0; pos<=180; pos++) {
+//     myservo.write(pos);
+//     delay(10);
+//   }
+//   for (int pos = 180; pos>=0; pos--) {
+//     myservo.write(pos);
+//     delay(10);
+//   }
+// }
+
+void loop() {
+  myservo.write(180);
+  delay(2000);
+  myservo.write(0);
+  delay(2000);
+  myservo.write(180);
+  delay(2000);
+}
